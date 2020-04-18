d9d0318ed177968a4f41375f15b25e180aa77997
Arihant Singla <41896258+arihantsingla@users.noreply.github.com>
2020-04-18 12:30:45 -0700

    Create BinaryTreeFromArray.java

---
+++ Trees/BinaryTreeFromArray.java
@@ -0,0 +1,60 @@
+import java.util.*;
+class Node
+{
+  int data; // data used as key value
+  Node leftChild;
+  Node rightChild;
+  public Node()
+  {
+    data=0;
+  }
+  public Node(int d)
+  {
+    data=d;
+  }
+}
+class Main
+{
+      
+  static void inOrder(Node root)
+  {
+    if (root != null)
+    {
+      inOrder(root.leftChild);
+      System.out.print(root.data + " ");
+      inOrder(root.rightChild);
+    }
+  }
+  static Node buildTree(int t[], int x)
+{
+  Node node = new Node(t[0]);
+  java.util.Queue<Node> n = new java.util.LinkedList<>();
+  n.add(node);
+  for(int i=1;i<t.length;++i){
+     Node nn=new Node(t[i]);
+      Node q=n.peek();
+      if(q.leftChild==null){
+        q.leftChild=nn;
+        n.add(q.leftChild);
+      }
+      
+      else if(q.rightChild==null) { 
+        q.rightChild=nn;
+        n.remove();
+        n.add(q.rightChild);
+      }
+    }
+  return node;
+}
+public static void main(String[] args)
+  {
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;++i)
+      a[i]=sc.nextInt();
+    Node root = null;
+    root = buildTree(a,n);
+   inOrder(root);
+  }
+}
